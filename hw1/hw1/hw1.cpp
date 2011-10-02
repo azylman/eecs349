79a38fab0952e30fb57b0cf38204aae05d452115
Alex Zylman <alex@zylman.com>
2011-10-02 14:51:48 -0500

    hello world

---
+++ hw1/hw1/hw1.cpp
@@ -0,0 +1,13 @@
+// hw1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using std::cout;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!";
+	return 0;
+}
+
