01a0aa988b152b9715af1941b9a5d1f9edbffedd
Alex Zylman <alex@zylman.com>
2011-10-03 01:45:44 -0500

    read in our items from our dataset file

--- hw1-managed/hw1-managed/hw1-managed.cpp
+++ hw1-managed/hw1-managed/hw1-managed.cpp
@@ -1,6 +1,7 @@
 // hw1-managed.cpp : main project file.
 
 #include "stdafx.h"
+#include "Item.h"
 
 using namespace System;
 using namespace System::Collections::Generic;
@@ -13,12 +14,31 @@ int main(array<String^> ^args)
 		Console::WriteLine("Error! Must have four arguments: inputFileName, trainingSetSize, numberOfTrials, verbose");
 		return -1;
 	}
+
+	// Initialize our parameters
     String^ inputFileName = argsList[0];
 	int trainingSetSize = Convert::ToInt32(argsList[1]);
 	int numberOfTrials = Convert::ToInt32(argsList[2]);
 	bool verbose = Convert::ToBoolean(argsList[3]);
 
-	
+	// Read in the dataset
+	StreamReader^ dataset = gcnew StreamReader(inputFileName);
+
+	// Create our items list from our input
+	List<String^>^ attributeNames = gcnew List<String^>(dataset->ReadLine()->Split('\t'));
+	List<Item^>^ items = gcnew List<Item^>();
+	while (String^ item = dataset->ReadLine()) {
+		List<String^>^ attributes = gcnew List<String^>(item->Split('\t'));
+		Dictionary<String^, String^>^ attributeDict = gcnew Dictionary<String^, String^>();
+		if (attributes->Count != attributeNames->Count) {
+			Console::WriteLine("Malformed dataset - attribute size is not equal to the number of attribute names");
+			return -1;
+		}
+		for (int i = 0; i < attributes->Count; ++i) {
+			attributeDict->Add(attributeNames[i], attributes[i]);
+		}
+		items->Add(gcnew Item(attributeDict));
+	}
 
     return 0;
 }
