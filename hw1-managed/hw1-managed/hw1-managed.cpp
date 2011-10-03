938f4dc377b80f286494024a920d05c7b0dc67ec
Alex Zylman <alex@zylman.com>
2011-10-03 01:30:05 -0500

    added command line options

--- hw1-managed/hw1-managed/hw1-managed.cpp
+++ hw1-managed/hw1-managed/hw1-managed.cpp
@@ -3,9 +3,22 @@
 #include "stdafx.h"
 
 using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::IO;
 
-int main(array<System::String ^> ^args)
+int main(array<String^> ^args)
 {
-    Console::WriteLine(L"Hello World");
+	List<String^>^ argsList = gcnew List<String^>(args);
+	if (argsList->Count != 4) {
+		Console::WriteLine("Error! Must have four arguments: inputFileName, trainingSetSize, numberOfTrials, verbose");
+		return -1;
+	}
+    String^ inputFileName = argsList[0];
+	int trainingSetSize = Convert::ToInt32(argsList[1]);
+	int numberOfTrials = Convert::ToInt32(argsList[2]);
+	bool verbose = Convert::ToBoolean(argsList[3]);
+
+	
+
     return 0;
 }
