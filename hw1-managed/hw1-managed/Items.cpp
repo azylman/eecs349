e39a1214b7465e6805eb34fcc4604bf8932468f8
Alex Zylman <alex@zylman.com>
2011-10-03 02:54:36 -0500

    add a method to calculate entropy gain for an attribute

--- hw1-managed/hw1-managed/Items.cpp
+++ hw1-managed/hw1-managed/Items.cpp
@@ -2,7 +2,7 @@
 #include "Items.h"
 
 using namespace System;
-using namespace System;
+using namespace System::Collections::Generic;
 
 Items::Items(List<String^>^ attributes) : attributes(attributes) {
 	items = gcnew List<Item^>();
@@ -14,10 +14,69 @@ double Items::calculateEntropy(int positive, int negative) {
 	return -good * Math::Log(good, 2) - bad * Math::Log(bad, 2);
 }
 
+double Items::calculateEntropyGainFromAttribute(Dictionary<String^, String^>^ usedClassifications, String^ attribute) {
+	List<Item^>^ filteredItems = gcnew List<Item^>();
+	if (usedClassifications->Count > 0) {
+		for each(Item^ item in items) {
+			bool add = true;
+			for each(KeyValuePair<String^, String^>^ classification in usedClassifications) {
+				if (!item->GetAttribute(classification->Key)->Equals(classification->Value)) {
+					add = false;
+				}
+			}
+			if (add) {
+				filteredItems->Add(item);
+			}
+		}
+	} else {
+		filteredItems = items;
+	}
+
+	int originalGood = 0;
+	int originalBad = 0;
+	Dictionary<String^, int>^ goodCountsByAttribute = gcnew Dictionary<String^, int>();
+	Dictionary<String^, int>^ badCountsByAttribute = gcnew Dictionary<String^, int>();
+	Dictionary<String^, int>^ totalItemsByAttribute = gcnew Dictionary<String^, int>();
+
+	for each (Item^ item in filteredItems) {
+		if (item->GetAttribute("CLASS")->Equals("1")) {
+			originalGood++;
+
+			addOrIncrementKey(goodCountsByAttribute, item->GetAttribute(attribute));
+			addOrIncrementKey(totalItemsByAttribute, item->GetAttribute(attribute));
+		} else if (item->GetAttribute("CLASS")->Equals("0")) {
+			originalBad++;
+
+			addOrIncrementKey(badCountsByAttribute, item->GetAttribute(attribute));
+			addOrIncrementKey(totalItemsByAttribute, item->GetAttribute(attribute));
+		} else {
+			// We're assuming that our dataset is valid - otherwise, this should throw an exception.
+		}
+	}
+
+	int totalItems = originalGood + originalBad;
+
+	double entropy = calculateEntropy;
+	for each (KeyValuePair<String^, int> attributeCount in totalItemsByAttribute) {
+		String^ key = attributeCount->Key;
+		int value = attributeCount->Value
+		entropy -= key / totalItems * calculateEntropy(goodCountsByAttribute[value], badCountsByAttribute[value]);
+	}
+	return entropy;
+}
+
+void Items::addOrIncrementKey(Dictionary^<String^, int> dict, String^ key) {
+	if (dict->ContainsKey(key)) {
+		dict[key]++;
+	} else {
+		dict->Add(key, 1);
+	}
+}
+
 void Items::Add(Item^ item) {
 	items->Add(item);
 }
 
-String^ Items::getBestClassifer(Dictionary<String^, String^> usedClassifications) {
+String^ Items::getBestClassifer(Dictionary<String^, String^>^ usedClassifications) {
 	return "";
 }
--- hw1-managed/hw1-managed/Items.h
+++ hw1-managed/hw1-managed/Items.h
@@ -8,9 +8,11 @@ private:
 	List<Item^>^ items;
 	List<String^>^ attributes;
 	double calculateEntropy(int positive, int negative);
+	double calculateEntropyGainFromAttribute(Dictionary<String^, String^>^ usedClassifications, String^ attribute);
+	void addOrIncrementKey(Dictionary^<String^, int> dict, String^ key);
 public:
 	Items(List<String^>^ attributes);
 	void Add(Item^ item);
-	String^ getBestClassifer(Dictionary<String^, String^> usedClassifications);
+	String^ getBestClassifer(Dictionary<String^, String^>^ usedClassifications);
 };
 
