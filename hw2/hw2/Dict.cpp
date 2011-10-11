3ec02f2827133eeefd967bd6b089eb5d5185eddd
Alex Zylman <alex@zylman.com>
2011-10-11 17:00:42 -0500

    added a cache for costs we've already looked up

--- hw2/hw2/Dict.cpp
+++ hw2/hw2/Dict.cpp
@@ -5,6 +5,7 @@ using namespace System::IO;
 
 Dict::Dict(String^ dictPath) {
 	dict = gcnew HashSet<String^>();
+	costs = gcnew Dictionary<String^, Dictionary<String^, int>^>();
 
 	// Read in the dictionary
 	StreamReader^ dictFile = gcnew StreamReader(dictPath);
@@ -20,4 +21,36 @@ String^ Dict::getCorrectWord(String^ word) {
 	}
 
 	// Find the word with the smallest Levenshtein Distance
+	return "";
+}
+
+void Dict::addCost(String^ word1, String^ word2, int cost) {
+	if (costs->ContainsKey(word1)) {
+		Dictionary<String^, int>^ matches = costs[word1];
+		matches->Add(word2, cost);
+	} else {
+		Dictionary<String^, int>^ matches = gcnew Dictionary<String^, int>();
+		matches->Add(word2, cost);
+		costs->Add(word1, matches);
+	}
+
+	if (costs->ContainsKey(word2)) {
+		Dictionary<String^, int>^ matches = costs[word2];
+		matches->Add(word1, cost);
+	} else {
+		Dictionary<String^, int>^ matches = gcnew Dictionary<String^, int>();
+		matches->Add(word1, cost);
+		costs->Add(word2, matches);
+	}
+}
+
+int Dict::getCost(String^ word1, String^ word2) {
+	if (costs->ContainsKey(word1)) {
+		Dictionary<String^, int>^ matches = costs[word1];
+		if (costs->ContainsKey(word2)) {
+			return matches[word2];
+		}
+	}
+
+	return -1;
 }
--- hw2/hw2/Dict.h
+++ hw2/hw2/Dict.h
@@ -5,7 +5,11 @@ using namespace System::Collections::Generic;
 
 public ref class Dict
 {
+private:
 	HashSet<String^>^ dict;
+	Dictionary<String^, Dictionary<String^, int>^>^ costs;
+	void addCost(String^ word1, String^ word2, int cost);
+	int getCost(String^ word1, String^ word2);
 public:
 	Dict(String^ dictPath);
 	String^ getCorrectWord(String^ word);
