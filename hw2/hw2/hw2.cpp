d898cf3f6b1ce7e30e04bee10491850eab8602d7
Alex Zylman <alex@zylman.com>
2011-10-12 15:49:45 -0500

    added timing tests

--- hw2/hw2/hw2.cpp
+++ hw2/hw2/hw2.cpp
@@ -4,18 +4,37 @@
 #include "Dict.h"
 
 using namespace System;
+using namespace System::IO;
+using namespace System::Diagnostics;
 
 int main(array<System::String ^> ^args)
 {	
 	String^ path = "../";
 	// Create our file paths
     String^ dictPath = path + "3esl.txt";
+	String^ testingPath = path + "wikipediatypoclean.txt";
 
 	Dict^ dict = gcnew Dict(dictPath);
+	Dictionary<String^, String^>^ testingData = gcnew Dictionary<String^, String^>();
 
-	Console::WriteLine(dict->getCorrectWord("ablyl"));
+	StreamReader^ testingDataFile = gcnew StreamReader(testingPath);
 
-	Console::WriteLine(dict->getCorrectWord("ablyl"));
+	while (String^ data = testingDataFile->ReadLine()) {
+		List<String^>^ parts = gcnew List<String^>(data->Split('\t'));
+		testingData->Add(parts[0], parts[1]);
+	}
+
+	for each (KeyValuePair<String^, String^>^ testData in testingData) {
+		Stopwatch^ timer = Stopwatch::StartNew();
+		String^ result = dict->getCorrectWord(testData->Key);
+		long time = timer->ElapsedMilliseconds;
+		Console::Write(testData->Key + " took " + time + " milliseconds to evaluate to " + result + " ");
+		if (result == testData->Value) {
+			Console::WriteLine("which is CORRECT!");
+		} else {
+			Console::WriteLine("but it should be " + testData->Value);
+		}
+	}
 
     Console::WriteLine(L"Hello World");
     return 0;
