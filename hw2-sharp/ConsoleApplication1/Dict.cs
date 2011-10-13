cf4934bf597fc03ecd0f7653b3a0582268d29128
Alex Zylman <alex@zylman.com>
2011-10-12 19:48:37 -0500

    added much faster c# version

---
+++ hw2-sharp/ConsoleApplication1/Dict.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Dict
+    {
+	    private HashSet<String> dict;
+	    private Dictionary<String, String> correctWords;
+
+        private int calculateLevenshteinDistance(String s, String t)
+        {
+            int m = s.Length;
+	        int n = t.Length;
+
+            int[,] d = new int[m + 1, n + 1];
+	        int deletionCost = 1;
+	        int insertionCost = 1;
+	        int substitutionCost = 1;
+
+	        for (int i = 0; i < m; ++i) {
+                d[i, 0] = i;
+	        }
+	        for (int i = 0; i < n; ++i) {
+                d[0, i] = i;
+	        }
+
+	        for (int j = 0; j < n; ++j) {
+		        for (int i = 0; i < m; ++i) {
+			        if (s[i] == t[j]) {
+                        int cost = d[i, j];
+				        d[i + 1, j + 1] = cost;
+			        } else {
+				        int cost = Math.Min(
+								        Math.Min(
+                                            d[i, j + 1] + deletionCost,
+									        d[i + 1, j] + insertionCost),
+								        d[i, j] + substitutionCost);
+                        d[i + 1, j + 1] = cost;
+			        }
+		        }
+	        }
+
+	        return d[m - 1, n - 1];
+        }
+
+        public Dict(String dictPath)
+        {
+            dict = new HashSet<String>();
+	        correctWords = new Dictionary<String, String>();
+
+	        StreamReader dictFile = new StreamReader(dictPath);
+
+            String item;
+	        while ((item = dictFile.ReadLine()) != null) {
+		        dict.Add(item);
+	        }
+        }
+        public String getCorrectWord(String word)
+        {
+	        if (dict.Contains(word)) {
+		        return word;
+	        }
+
+	        if (correctWords.ContainsKey(word)) {
+		        return correctWords[word];
+	        }
+
+	        // Initialize this to the largest possible distance
+	        int shortestDistance = int.MaxValue;
+	        String bestWord = "";
+	        foreach (String alternateWord in dict) {
+		        //int cost = getCost(word, alternateWord);
+                int cost = 0;
+		        // 0 is the placeholder for "not found". It will never be an actual result
+		        // because a distance of 0 would signify that our word was in the dictionary,
+		        // and we're checking that above with dict.Contains
+		        if (cost == 0) {
+			        cost = calculateLevenshteinDistance(word, alternateWord);
+		        }
+
+		        if (cost < shortestDistance) {
+			        bestWord = alternateWord;
+			        shortestDistance = cost;
+		        }
+	        }
+
+            correctWords.Add(word, bestWord);
+	        return bestWord;
+        }
+    }
+}
---
+++ hw2-sharp/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "";
+	        // Create our file paths
+            String dictPath = path + "3esl.txt";
+	        String testingPath = path + "wikipediatypoclean.txt";
+
+	        Dict dict = new Dict(dictPath);
+	        Dictionary<String, String> testingData = new Dictionary<String, String>();
+
+	        StreamReader testingDataFile = new StreamReader(testingPath);
+
+            String data;
+	        while ((data = testingDataFile.ReadLine()) != null) {
+		        List<String> parts = new List<String>(data.Split('\t'));
+		        testingData.Add(parts[0], parts[1]);
+	        }
+
+            int success = 0;
+            int total = 0;
+            Stopwatch timer = Stopwatch.StartNew();
+	        foreach (KeyValuePair<String, String> testData in testingData) {
+		        String result = dict.getCorrectWord(testData.Key);
+		        //Console.Write(testData.Key + " took " + time + " milliseconds to evaluate to " + result + " ");
+		        if (result == testData.Value) {
+			        //Console.WriteLine("which is CORRECT!");
+                    success++;
+		        } else {
+			        //Console.WriteLine("but it should be " + testData.Value);
+		        }
+                total++;
+	        }
+            long time = timer.ElapsedMilliseconds;
+
+            Console.WriteLine((double) success/(double) total + " success rate in " + time + " milliseconds.");
+        }
+    }
+}
