a5998e67e19449291da9141855779cd4640d33aa
Alex Zylman <alex@zylman.com>
2011-10-13 22:52:01 -0500

    added a Fold class

---
+++ hw2-sharp/ConsoleApplication1/Fold.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Fold
+    {
+        public Dictionary<String, String> trainingSet;
+        public Dictionary<String, String> testingSet;
+        public Fold(Dictionary<String, String> trainingSet, Dictionary<String, String> testingSet)
+        {
+            this.trainingSet = trainingSet;
+            this.testingSet = testingSet;
+        }
+    }
+}
